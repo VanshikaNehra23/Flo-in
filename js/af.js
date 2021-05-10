0894c643a471cf6b5f7350810dc09666b8c5ae80
Vanshika Nehra <72328934+VanshikaNehra23@users.noreply.github.com>
2021-05-10 23:11:41 +0530

    Add files via upload

---
+++ js/af.js
@@ -0,0 +1,31 @@
+(function () {
+    Array.prototype.forEach.call(document.querySelectorAll('form input[type=password]'), function (pass) {
+        var form = pass.closest('form');
+
+        // <input> and <input type=""> do not get picked up by input[type=text] selector
+        // so we must select all inputs and filter it with the .type property
+        var users = Array.prototype.filter.call(form.querySelectorAll('input'), function (input) {
+            return input.type == 'text' || input.type == 'email';
+        });
+
+        if (users.length == 1) {
+            var user = users[0];
+
+            chrome.runtime.onMessage.addListener(function (message, sender, respond) {
+                if (message.from == 'background') {
+                    if (message.action == 'do_fill') {
+                        user.value = message.user;
+                        pass.value = message.pass;
+
+                        user.style.backgroundColor = '#edffe3';
+                        pass.style.backgroundColor = '#edffe3';
+                    }
+                }
+            });
+            chrome.runtime.sendMessage('', {
+                from: 'content_script',
+                action: 'fill_available'
+            });
+        }
+    });
+})();
